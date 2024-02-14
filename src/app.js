2729bfcb97bad62524cfbeffee45cc95ba272000
Lydia1596 <lydiaprentice@hotmail.co.uk>
2024-02-14 13:03:37 +0000

    Added JavaScript and Typewriter Plugin

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings: "Hello World",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let poemForm = document.querySelector("#poem-form");
+poemForm.addEventListener("submit", generatePoem);
