25ed3410024213ff16c77f9d4544a39d6a186a3b
Lydia1596 <lydiaprentice@hotmail.co.uk>
2024-02-15 12:13:41 +0000

    Improved UX

--- src/app.js
+++ src/app.js
@@ -16,8 +16,12 @@ function generatePoem(event) {
   let apiKey = "34f2a007t4073b7ac44010a0e7f0b6co";
   let prompt = `The instructions are to generate a poem about ${instructionsInput.value}`;
   let context =
-    "Knowledgeable and creative AI assistant, please generate a 4 line poem in basic HTML format, font-size 14px and separate each line with a <br/>. Do not include a <br/> on the last line. Do not include a title to the poem. Make sure to follow the instructions.";
+    "Knowledgeable and creative AI assistant, please generate a 4 line poem in basic HTML format, font-size 14px and separate each line with a <br/>. Do not include a <br/> on the final line. Do not include a title to the poem. Make sure to follow the instructions.";
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.add("poem");
+  poemElement.innerHTML = `Generating a poem about ${instructionsInput.value}...`;
 
   console.log(`Prompt: ${prompt}`);
 
